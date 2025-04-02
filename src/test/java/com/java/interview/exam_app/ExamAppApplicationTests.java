88a239a88e9b38fe5c5160a6290961a5aa29790b
Sachin Santhosh <sachinms.mec@gmail.com>
2025-04-03 00:34:52 +0530

    1st commit

---
+++ src/test/java/com/java/interview/exam_app/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.interview.exam_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
